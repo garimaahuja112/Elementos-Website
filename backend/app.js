2b66b290e61674c835db6783b0d6c853b40d74aa
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-02-06 19:24:30 +0530

    Added database

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+const app=express();
+
+mongoose.connect('mongodb+srv://mokshmahajan2004:xui8KeYMtbTN58dU@cluster0.cywnbpd.mongodb.net/?retryWrites=true&w=majority').then(()=>app.listen(5000)).then(()=>console.log("Connected To Database and Listening to Localhost 5000")).catch((err)=>console.log(err));
+
