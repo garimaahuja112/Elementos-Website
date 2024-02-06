fc5680258228ba1fb13b65b5f1000da2591536b2
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-02-06 22:39:25 +0530

    Added Schema

---
+++ backend/model/user.js
@@ -0,0 +1,26 @@
+ import mongoose from 'mongoose';
+ 
+ const Schema=mongoose.Schema;
+ const userScheme=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    branch:{
+        type:String,
+        required:true
+    },
+    year:{
+        type:Number,
+        required:true
+    },
+    event:{
+        
+    }
+ });
+ export default mongoose.model("user",userScheme);
